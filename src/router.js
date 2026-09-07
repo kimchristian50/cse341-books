c915bbf0188f224e26c462f62f62fa6146e20e73
kimchristian50 <pchristison@byupathway.edu>
2026-09-07 12:28:22 -0600

    added Swagger

--- src/router.js
+++ src/router.js
@@ -3,7 +3,47 @@ import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all books
+ *     description: Retrieves a list of all books.
+ *     responses:
+ *       200:
+ *         description: A list of books was successfully retrieved.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get a book by ID
+ *     description: Retrieves a single book using its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the book to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The requested book was successfully retrieved.
+ *       400:
+ *         description: Invalid book ID.
+ *       404:
+ *         description: Book not found.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/books/:id', getBookByIdHandler);
 
 
